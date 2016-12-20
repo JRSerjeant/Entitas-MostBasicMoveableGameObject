1b40c942f68c8e621258fdb17ee7904fd752f3da
Jack Serjeant <Jack.Serjeant@gmail.com>
2016-12-20 08:04:49 +0000

    Made object move

--- Assets/_Systems/MovableSystem.cs
+++ Assets/_Systems/MovableSystem.cs
@@ -1,17 +1,23 @@
 ﻿using System;
 using Entitas;
+using UnityEngine;
 
 class MovableSystem : IExecuteSystem, ISetPool
 {
     Pool _pool;
-
+    Group _Moveable;
     public void Execute()
     {
+        foreach (var member in _Moveable.GetEntities())
+        {
+            member.gameObject.gameObject.transform.Translate(Vector3.left * Time.deltaTime);
+        }
     }
 
     public void SetPool(Pool pool)
     {
         _pool = pool;
+        _Moveable = pool.GetGroup(CoreMatcher.Moveable);
     }
 }
 
